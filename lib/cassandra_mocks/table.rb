7d30e55582a278477d1be7381c259547daaa102f
Thomas Rogers <thomasrogers03@gmail.com>
2015-09-10 22:12:12 -0400

    Begin work on inserting records into Tables

--- lib/cassandra_mocks/table.rb
+++ lib/cassandra_mocks/table.rb
@@ -1,11 +1,37 @@
 module Cassandra
   module Mocks
     class Table < Cassandra::Table
-
       def initialize(keyspace, name, partition_key, clustering_key, fields)
         compaction = Cassandra::Table::Compaction.new('mock', {})
         options = Cassandra::Table::Options.new({}, compaction, {}, false, 'mock')
-        super(keyspace, name, partition_key, clustering_key, fields, options, [])
+        column_map = column_map(partition_key, clustering_key, fields)
+        super(keyspace, name, partition_key, clustering_key, column_map, options, [])
+      end
+
+      def insert(attributes)
+        attributes.keys.each do |column|
+          unless column_names.include?(column)
+            raise Errors::InvalidError.new("Invalid column, #{column}, specified", 'MockStatement')
+          end
+        end
+
+        rows << attributes
+      end
+
+      def rows
+        @rows ||= []
+      end
+
+      private
+
+      def column_names
+        columns.map(&:name)
+      end
+
+      def column_map(partition_key, clustering_key, fields)
+        (partition_key + clustering_key + fields).inject({}) do |memo, column|
+          memo.merge!(column.name => column)
+        end
       end
 
     end
--- spec/classes/table_spec.rb
+++ spec/classes/table_spec.rb
@@ -5,32 +5,71 @@ module Cassandra
     describe Keyspace do
 
       let(:keyspace_name) { 'keyspace' }
-      let(:keyspace) { Keyspace.new(keyspace_name) }
       let(:name) { 'table' }
       let(:pk_part_one) { Cassandra::Column.new('pk1', 'int', :asc) }
       let(:ck_part_one) { Cassandra::Column.new('ck1', 'string', :desc) }
       let(:fields_part_one) { Cassandra::Column.new('field1', 'double', :asc) }
+      let(:pk_part_two) { Cassandra::Column.new('pk1', 'double', :asc) }
+      let(:ck_part_two) { Cassandra::Column.new('ck1', 'int', :desc) }
+      let(:fields_part_two) { Cassandra::Column.new('field1', 'string', :asc) }
       let(:partition_key) { [pk_part_one] }
       let(:clustering_key) { [ck_part_one] }
       let(:fields) { [fields_part_one] }
+      let(:column_map) do
+        (partition_key + clustering_key + fields).inject({}) do |memo, column|
+          memo.merge!(column.name => column)
+        end
+      end
       let(:compaction) { Cassandra::Table::Compaction.new('mock', {}) }
       let(:options) { Cassandra::Table::Options.new({}, compaction, {}, false, 'mock') }
-      let(:super_table) { Cassandra::Table.new(keyspace, name, partition_key, clustering_key, fields, options, []) }
+      let(:super_table) { Cassandra::Table.new(keyspace_name, name, partition_key, clustering_key, column_map, options, []) }
 
-      subject { Table.new(keyspace, name, partition_key, clustering_key, fields) }
+      subject { Table.new(keyspace_name, name, partition_key, clustering_key, fields) }
 
       it { is_expected.to eq(super_table) }
 
       context 'with a different table configuration' do
         let(:name) { 'broken table' }
-        let(:pk_part_two) { Cassandra::Column.new('pk1', 'double', :asc) }
-        let(:ck_part_two) { Cassandra::Column.new('ck1', 'int', :desc) }
-        let(:fields_part_two) { Cassandra::Column.new('field1', 'string', :asc) }
         let(:partition_key) { [pk_part_one, pk_part_two] }
         let(:clustering_key) { [ck_part_one, ck_part_two] }
         let(:fields) { [fields_part_one, fields_part_two] }
 
         it { is_expected.to eq(super_table) }
+      end
+
+      describe '#insert' do
+        let(:attributes) { { 'pk1' => 15, 'ck1' => 'hello world' } }
+
+        it 'should create a record from the input row' do
+          subject.insert(attributes)
+          expect(subject.rows).to eq([attributes])
+        end
+
+        context 'with multiple records' do
+          let(:other_attributes) { { 'pk1' => 45, 'ck1' => 'goodbye', 'field1' => 'world' } }
+
+          it 'should be able to store multiple records' do
+            subject.insert(attributes)
+            subject.insert(other_attributes)
+            expect(subject.rows).to match_array([attributes, other_attributes])
+          end
+        end
+
+        context 'with a record containing invalid columns' do
+          let(:attributes) { { 'pk1' => 15, 'ck1' => 'hello world', 'field2' => 'stuff' } }
+
+          it 'should raise an error' do
+            expect { subject.insert(attributes) }.to raise_error(Cassandra::Errors::InvalidError, 'Invalid column, field2, specified')
+          end
+
+          context 'with an additional invalid column' do
+            let(:attributes) { { 'field3' => 'garbage', 'pk1' => 15, 'ck1' => 'hello world', 'field2' => 'stuff' } }
+
+            it 'should raise the error on the first invalid column found' do
+              expect { subject.insert(attributes) }.to raise_error(Cassandra::Errors::InvalidError, 'Invalid column, field3, specified')
+            end
+          end
+        end
       end
 
     end
