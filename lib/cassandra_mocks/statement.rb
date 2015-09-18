8a05d5313b5f4e1637cd7b93daef5e9e188b4bfa
Thomas Rogers <thomasrogers03@gmail.com>
2015-09-18 18:11:42 -0400

    Refactor Statement insert parsing

--- lib/cassandra_mocks/statement.rb
+++ lib/cassandra_mocks/statement.rb
@@ -11,25 +11,28 @@ module Cassandra
 
         tokens = Tokenizer.new(@cql).token_queue
         type = tokens.pop
-        if type.insert?
-          3.times { tokens.pop }
-
-          insert_keys = []
-          until (key = tokens.pop).rparen?
-            insert_keys << key.value unless key.comma?
-          end
-
-          2.times { tokens.pop }
-          insert_values = []
-          until (key = tokens.pop).rparen?
-            insert_values << key.value unless key.comma?
-          end
-
-          @args = insert_args(insert_keys, insert_values, args)
-        end
+        parse_insert_query(args, tokens) if type.insert?
       end
 
       private
+
+      def parse_insert_query(args, tokens)
+        3.times { tokens.pop }
+
+        insert_keys = parenthesis_values(tokens)
+        2.times { tokens.pop }
+        insert_values = parenthesis_values(tokens)
+
+        @args = insert_args(insert_keys, insert_values, args)
+      end
+
+      def parenthesis_values(tokens)
+        [].tap do |insert_values|
+          until (key = tokens.pop).rparen?
+            insert_values << key.value unless key.comma?
+          end
+        end
+      end
 
       def insert_args(insert_keys, insert_values, args)
         param_index = -1
