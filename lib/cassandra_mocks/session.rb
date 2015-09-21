4f37eae4aad252e7943792d2ef40d64a28e77301
Thomas Rogers <thomasrogers03@gmail.com>
2015-09-21 18:10:14 -0400

    Implement Session#add_keyspace

--- lib/cassandra_mocks/session.rb
+++ lib/cassandra_mocks/session.rb
@@ -1,6 +1,16 @@
 module Cassandra
   module Mocks
     class Session
+      attr_reader :keyspaces
+
+      def initialize
+        @keyspaces = []
+      end
+
+      def add_keyspace(name)
+        keyspaces << Keyspace.new(name)
+      end
+
       def prepare_async(cql)
         Cassandra::Future.value(Statement.new(cql, []))
       end
