97cd18d962de5340a55fcfd8d21a5b6b9fb85cfa
Thomas Rogers <thomasrogers03@gmail.com>
2015-09-21 18:44:31 -0400

    Implement support on the Keyspace for adding tables based off of Statements

--- lib/cassandra_mocks/keyspace.rb
+++ lib/cassandra_mocks/keyspace.rb
@@ -4,7 +4,19 @@ module Cassandra
 
       def initialize(name)
         replication = Cassandra::Keyspace::Replication.new('mock', {})
-        super(name, false, replication, [])
+        super(name, false, replication, {})
+      end
+
+      def add_table(name, primary_key, columns)
+        partition_key = primary_key.shift
+        partition_key_columns = partition_key.map { |name| Cassandra::Column.new(name, columns[name], :asc) }
+        clustering_columns = primary_key.map { |name| Cassandra::Column.new(name, columns[name], :asc) }
+        fields = columns.except(partition_key + primary_key).map { |name, type| Cassandra::Column.new(name, type, :asc) }
+        @tables[name] = Cassandra::Mocks::Table.new(self.name,
+                                             name,
+                                             partition_key_columns,
+                                             clustering_columns,
+                                             fields)
       end
 
     end
--- spec/classes/keyspace_spec.rb
+++ spec/classes/keyspace_spec.rb
@@ -6,7 +6,7 @@ module Cassandra
 
       let(:name) { 'keyspace' }
       let(:replication) { Cassandra::Keyspace::Replication.new('mock', {}) }
-      let(:super_keyspace) { Cassandra::Keyspace.new(name, false, replication, []) }
+      let(:super_keyspace) { Cassandra::Keyspace.new(name, false, replication, {}) }
 
       subject { Keyspace.new(name) }
 
@@ -17,6 +17,78 @@ module Cassandra
 
         it { is_expected.to eq(super_keyspace) }
       end
+
+      describe '#add_table' do
+        let(:columns) { {'pk1' => 'text', 'ck1' => 'int', 'field1' => 'double'} }
+        let(:partition_key) { {'pk1' => 'text'} }
+        let(:clustering_columns) { {'ck1' => 'int'} }
+        let(:fields) { {'field1' => 'double'} }
+        let(:primary_key) { [partition_key.keys, *clustering_columns.keys] }
+        let(:table_name) { 'generic_table' }
+        let(:table) do
+          Cassandra::Mocks::Table.new(name, table_name, create_columns(partition_key), create_columns(clustering_columns), create_columns(fields))
+        end
+
+        it 'should create a table using the specified parameters' do
+          subject.add_table(table_name, primary_key, columns)
+          expect(subject.table(table_name)).to eq(table)
+        end
+
+        context 'with a different keyspace name' do
+          let(:name) { 'production_keys' }
+
+          it 'should create a table using the specified parameters' do
+            subject.add_table(table_name, primary_key, columns)
+            expect(subject.table(table_name)).to eq(table)
+          end
+        end
+
+        context 'with a different table name' do
+          let(:table_name) { 'books' }
+
+          it 'should create a table using the specified parameters' do
+            subject.add_table(table_name, primary_key, columns)
+            expect(subject.table(table_name)).to eq(table)
+          end
+        end
+
+        context 'with a different partition key' do
+          let(:columns) { {'genre' => 'text', 'style' => 'text', 'ck1' => 'int', 'field1' => 'double'} }
+          let(:partition_key) { {'genre' => 'text', 'style' => 'text'} }
+
+          it 'should create a table using the specified parameters' do
+            subject.add_table(table_name, primary_key, columns)
+            expect(subject.table(table_name).to_cql).to eq(table.to_cql)
+          end
+        end
+
+        context 'with different clustering columns' do
+          let(:columns) { {'pk1' => 'text', 'location_number' => 'int', 'district' => 'text', 'field1' => 'double'} }
+          let(:clustering_columns) { {'district' => 'text', 'location_number' => 'int'} }
+
+          it 'should create a table using the specified parameters' do
+            subject.add_table(table_name, primary_key, columns)
+            expect(subject.table(table_name).to_cql).to eq(table.to_cql)
+          end
+        end
+
+        context 'with different fields' do
+          let(:columns) { {'pk1' => 'text', 'ck1' => 'int', 'green field' => 'double', 'blue field' => 'int'} }
+          let(:fields) { {'green field' => 'double', 'blue field' => 'int'} }
+
+          it 'should create a table using the specified parameters' do
+            subject.add_table(table_name, primary_key, columns)
+            expect(subject.table(table_name).to_cql).to eq(table.to_cql)
+          end
+        end
+      end
+
+      private
+
+      def create_columns(columns)
+        columns.map { |key, value| Cassandra::Column.new(key, value, :asc) }
+      end
+
     end
   end
 end
