e4067fc3925844a6e78dd13058b72f24f9b371ff
Thomas Rogers <thomasrogers03@gmail.com>
2015-09-22 18:04:54 -0400

    Add TRUNCATE token keyword

--- lib/cassandra_mocks/statement/tokenizer.rb
+++ lib/cassandra_mocks/statement/tokenizer.rb
@@ -5,6 +5,7 @@ module Cassandra
         #noinspection RubyStringKeysInHashInspection
         KEYWORD_MAP = {
             'CREATE' => :create,
+            'TRUNCATE' => :truncate,
             'PRIMARY' => :primary,
             'KEY' => :key,
             'TABLE' => :table,
